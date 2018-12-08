453bc956e62d2f903fc9086620bf11e73de728a7
20180912 <43218559+20180912@users.noreply.github.com>
2018-12-08 15:08:21 +0100

    update

--- outlier_detection.py
+++ outlier_detection.py
@@ -5,29 +5,29 @@ import matplotlib.pyplot as plt
 filename = 'train.gz'
  
 chunksize = 100000
-chunks=[]
+chunks = []
 for chunk in pd.read_csv(filename, compression='gzip', header=0, sep=',', usecols= ['hour', 'click'], index_col='hour', chunksize=chunksize):
+    #broadcasting 1 to count impressions. If totals aren't needed, the CTR can be calulated by taking the mean of clicks
     chunk['impression'] = 1
     #converting the index column to datetime and resampling
     time_format = '%y%m%d%H'
     chunk.index = pd.to_datetime(chunk.index, format=time_format)
-    chunk=chunk.resample('H').sum()
+    chunk = chunk.resample('H').sum()
     chunks.append(chunk)
  
 df = pd.concat(chunks)
 df = df.groupby(df.index).sum()
  
 """
-#The following simpler code works as well but requires more RAM
+#The following code works as well and is simpler but requires more RAM
 df = pd.read_csv(filename, compression='gzip', header=0, sep=',', usecols= ['hour', 'click'], index_col='hour')
 df['impression'] = 1
-#converting the index column to datetime and resampling
 time_format = '%y%m%d%H'
 df.index = pd.to_datetime(df.index, format=time_format)
 df=df.resample('H').sum()
 """
  
-df['CTR']=df['click']/df['impression']
+df['CTR'] = df['click']/df['impression']
 #calculating moving average with beginning fixed, alternatively use df['CTR'].rolling(window=...).mean() for a constant time frame
 df['CTR_moving_avg'] = df['CTR'].expanding().mean()
 df['CTR_moving_std'] = df['CTR'].expanding().std()
@@ -50,9 +50,8 @@ plt.plot(df['CTR'], color='k', linestyle = 'none', marker = '.')
 plt.plot(df['CTR_moving_avg'], color='b', linestyle='dashed')
 plt.plot(df['outlier'], color='r', linestyle = 'none', marker = '.')
 plt.fill_between(df.index,df['CTR_moving_avg']+1.5*df['CTR_moving_std'], df['CTR_moving_avg']-1.5*df['CTR_moving_std'], color='b', alpha=0.3)
-#datetime = df.index.strftime('%d')
-#df.index, #datetime)
 plt.tight_layout(pad=4, h_pad=4)
+#using the default x-axis. It should be possible to create different lables using df.index.strftime('...')
 plt.xticks(rotation=30)
 plt.title('Outliers')
 plt.legend(('CTR','$\overline{CTR}$', 'Outlier', '$1.5\cdot\sigma$'), loc='best')
@@ -60,6 +59,7 @@ plt.xlabel('day')
 plt.ylabel('CTR')
 plt.ylim(bottom=0)
 plt.style.use('ggplot')
- 
+
+#saving and plotting the charts 
 plt.savefig('output.png')
 plt.show()
